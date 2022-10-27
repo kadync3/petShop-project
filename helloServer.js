cd73a6692a9bee9f8490f79a592a0045a15d5388
kadync3 <106933582+kadync3@users.noreply.github.com>
2022-10-27 15:38:28 -0500

    sever functonal

--- helloServer.js
+++ helloServer.js
@@ -1,36 +1,40 @@
-'use strict';
-const fs = require('fs')
-const { createSecretKey } = require('crypto');
-const http = require('http');
+"use strict";
+const fs = require("fs");
+const { createSecretKey } = require("crypto");
+const http = require("http");
 const port = process.env.PORT || 8000;
-const petShop = require('./pets.js');
-console.log('at server')
-petShop()
+const petShop = require("./pets.js");
+console.log("at server");
+petShop();
 
-const server = http.createServer(function(req, res) {
-  if(req.url === '/pets'){
-    res.setHeader('Content-Type', 'application/JSON');
-    fs.readFile("pets.json",function (err, data) {
-      
+const server = http.createServer(function (req, res) {
+  var test = req.url.split("/").slice(1);
+  console.log(test);
+  var testNum = test[test.length - 1];
+  if (test[0] === "pets" && test[1] === undefined) {
+    res.setHeader("Content-Type", "application/JSON");
+    fs.readFile("pets.json", function (err, data) {
       let dataArr = JSON.parse(data);
       if (data) {
         res.end(JSON.stringify(dataArr));
       }
     });
-    
-  } else if (req.url === `/pets/1`){
-
-    fs.readFile("pets.json",function (err, data) {
+  } else if (test[0] === `pets` && test[1] === testNum) {
+    console.log(test[test.length - 1]);
+    fs.readFile("pets.json", function (err, data) {
       let dataArr = JSON.parse(data);
-    if (data) {
-      res.end(JSON.stringify(dataArr[1]));
-    }
-  })
-} 
+      if (dataArr[testNum] === undefined) {
+        res.end("Usage: node pets.js read INDEX");
+      }
+      if (data) {
+        res.end(JSON.stringify(dataArr[testNum]));
+      }
+    });
+  } else {
+    res.end("err dUmBDuMb");
+  }
 });
 
-
-
-server.listen(port, function() {
-  console.log('Listening on port', port);
+server.listen(port, function () {
+  console.log("Listening on port", port);
 });
